f1df2228b03d3d8244f42d0feea391f254ae4f55
Valentine Efagene <efagenevalentine@gmail.com>
2025-02-28 16:45:10 +0100

    cleanup: logs

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Logger, Post } from "@nestjs/common";
 import { OrderService } from "./order.service";
 import { CreateOrderDto } from "./order.dto";
+import { Ctx, RmqContext } from "@nestjs/microservices";
 
 @Controller('orders')
 export class OrderController {
+    private readonly logger = new Logger(OrderController.name);
+
     constructor(
         private readonly orderService: OrderService,
     ) { }
@@ -13,5 +16,11 @@ export class OrderController {
         @Body() dto: CreateOrderDto
     ) {
         return this.orderService.createOrder(dto);
+    }
+
+    @Get()
+    getOrders(
+    ) {
+        return this.orderService.getOrders();
     }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,10 +1,13 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { CreateOrderDto } from "./order.dto";
 import { InjectionToken, RabbitMqPatterns } from "src/app.enums";
 import { ClientProxy } from "@nestjs/microservices";
+import { timeout } from "rxjs";
 
 @Injectable()
 export class OrderService {
+    private readonly logger = new Logger(OrderService.name);
+
     constructor(
         @Inject(InjectionToken.RABBITMQ_SERVICE) private readonly rabbitMQService: ClientProxy
     ) { }
@@ -13,5 +16,11 @@ export class OrderService {
         this.rabbitMQService.emit(RabbitMqPatterns.ORDER_CREATED, dto);
 
         return { message: 'Order request sent' };
+    }
+
+    async getOrders() {
+        return this.rabbitMQService.send({
+            cmd: RabbitMqPatterns.FETCH_ORDERS
+        }, {}).pipe(timeout(5000));
     }
 }
